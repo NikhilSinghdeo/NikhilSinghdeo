ba5805462ab06d8078747d3bad56dd958914e60c
Nikhil_singhdeo <82502288+NikhilSinghdeo@users.noreply.github.com>
2021-04-15 15:53:56 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,28 @@
+import turtle
+
+a=5
+print(a)
+
+b='hello'
+print(b)
+
+credit_card=76582737536767
+
+print(credit_card)
+
+#square 
+
+def square():
+  qazi_turtle=turtle.Turtle()
+  qazi_turtle.forward(100)
+  qazi_turtle.right(90)
+  qazi_turtle.forward(100)
+  qazi_turtle.right(100)
+  qazi_turtle.forward(100)
+  qazi_turtle.right(100)
+  qazi_turtle.forward(100)
+
+
+square()
+qazi_turtle.forward(200)
+square()
